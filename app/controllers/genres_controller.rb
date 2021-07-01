ea342b1fa0a1f5144864b64331ee1840acf2baa9
rahimshahad <rahim.shahad2@gmail.com>
2021-07-01 13:47:11 -0400

    commit tree 77acd2c05224d58915d3dc4c2d90a69e91051eef

---
+++ app/controllers/genres_controller.rb
@@ -0,0 +1,33 @@
+class GenresController < ApplicationController
+    def index
+        @genre = Genre.all
+    end
+
+    def show
+        @genre = Genre.find_by_id(params[:id])
+    end
+
+    def new
+        @genre =Genre.new
+    end
+
+    def create
+        @genre = Genre.create(genre_params(:name))
+        redirect_to genre_path(@genre)
+    end
+
+    def edit
+        @genre = Genre.find_by_id(params[:id])
+    end
+
+    def update
+        @genre = Genre.find_by_id(params[:id])
+        @genre = Genre.update(genre_params(:name))
+        redirect_to genre_path(@genre)
+    end
+
+    private
+    def genre_params(*args)
+        params.require(:genre).permit(*args)
+    end
+end
